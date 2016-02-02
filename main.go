37e049515bef5760bc3c432f99d51ea4862075b3
Lukasz Jagiello <lukasz@wikia-inc.com>
2016-02-02 07:23:59 +0000

    flags support + go fmt ./...

--- main.go
+++ main.go
@@ -1,48 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "github.com/jeromer/syslogparser/rfc3164"
-    "github.com/google/gopacket"
-    "github.com/google/gopacket/pcap"
+	"flag"
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/jeromer/syslogparser/rfc3164"
+	"time"
 )
 
 var (
-    device      string = "eth3"
-    snapshotLen int32  = 1600
-    promiscuous bool   = false
-    err         error
-    timeout     time.Duration = 0 * time.Second
-    handle      *pcap.Handle
+	snapshotLen int32 = 1600
+	promiscuous bool  = false
+	err         error
+	timeout     time.Duration = 0 * time.Second
+	handle      *pcap.Handle
 )
 
 func main() {
-    handle, err := pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
-    if err != nil { panic(err) }
-    err = handle.SetBPFFilter("udp and port 500")
-    if err != nil { panic(err) }
-    defer handle.Close()
 
-    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-    for packet := range packetSource.Packets() {
-        printPacketInfo(packet)
-    }
+	device := flag.String("d", "eth0", "device")
+	port := flag.String("p", "514", "port")
+	flag.Parse()
+
+	handle, err := pcap.OpenLive(*device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		panic(err)
+	}
+	err = handle.SetBPFFilter("udp and port " + *port)
+	if err != nil {
+		panic(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		printPacketInfo(packet)
+	}
 }
 
 func printPacketInfo(packet gopacket.Packet) {
-    applicationLayer := packet.ApplicationLayer()
-    if applicationLayer != nil {
-        buff := []byte(applicationLayer.Payload())
-        p := rfc3164.NewParser(buff)
-        err := p.Parse()
-        if err != nil {
-            fmt.Println("Error decoding Payload:", err)
-        }
-        fmt.Printf("%s", p.Dump()["content"])
-    }
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+		buff := []byte(applicationLayer.Payload())
+		p := rfc3164.NewParser(buff)
+		err := p.Parse()
+		if err != nil {
+			fmt.Println("Error decoding Payload:", err)
+		}
+		fmt.Printf("%s", p.Dump()["content"])
+	}
 
-    if err := packet.ErrorLayer(); err != nil {
-        fmt.Println("Error decoding some part of the packet:", err)
-    }
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
 }
